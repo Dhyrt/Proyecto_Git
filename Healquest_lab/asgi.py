43d804568097ccbd60caae14a717a1fdec0b13ca
Dhyrt <caro.ahumada@duocuc.cl>
2024-05-20 16:44:36 -0400

    iniciacion base

--- Healquest_lab/asgi.py
+++ Healquest_lab/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Healquest_lab2 project.
+ASGI config for Healquest_lab project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Healquest_lab2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Healquest_lab.settings')
 
 application = get_asgi_application()
--- Healquest_lab/wsgi.py
+++ Healquest_lab/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Healquest_lab2 project.
+WSGI config for Healquest_lab project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Healquest_lab2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Healquest_lab.settings')
 
 application = get_wsgi_application()
